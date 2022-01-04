55522b1a7377c0150cdbe91c3a00f2d266d85fb9
OrangeFlavoredDerek <derekchan0407@gmail.com>
2022-01-04 15:38:01 +0800

    Initial Commit

---
+++ Wi-FiPassword/Wi_FiPasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wi_FiPasswordApp.swift
+//  Wi-FiPassword
+//
+//  Created by 陳峻琦 on 4/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct Wi_FiPasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
